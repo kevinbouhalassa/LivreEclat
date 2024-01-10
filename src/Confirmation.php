13f1897b4f2a28955a68818f1996c132c1f3e865
Kevin Bouhalassa <bouhalassak@gmail.com>
2024-01-09 20:04:04 -0500

    fonction envoi courriel form OK

--- src/Confirmation.php
+++ src/Confirmation.php
@@ -15,60 +15,33 @@ include './pages/partials/header.php';
 ?>
 <main>
     <?php
-    print_r($_POST);
-    /* if(isset($_POST['submit'])) {
-                    $email_to1 = "bouhalassak@gmail.com";
-                    $email_to2 = $_POST['courriel'];
-                    $email_subject = "Confirmation de la réservation";
-                    $email_from1 = "bouhalassak@gmail.com";
-                    $email_from2 = $_POST['courriel'];
-                    $prenom = $_POST['prenom'];
-                    $nom = $_POST['nom'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST['courriel'];
+    $prenom = $_POST['prenom'];
+    $nom = $_POST['nom'];
+    $emailAdmin = "info@livreeclat.com";
+    $toAdmin = "bouhalassak@gmail.com"; 
 
-                    function clean_str($string) {
-                        $bad = array("content-type" ,"bcc:","to:","cc:","href");
-                        return str_replace($bad,"",$string);
-                    }
-
-                    $emailAdmin = "Confirmation de la réservation de. \n\n";
-                    $emailAdmin .= "Prenom: ".$prenom. "\n";
-                    $emailAdmin .= "Nom: ".$nom. "\n";
-
-                    $emailClient = "Merci d'avoir choisi Livre Éclat. Votre livre" . $livres['Titre'] . "est maintenant réservé pour 30 jours. 
-                    Vous pouvez passer en magasin durant nos heures d'ouvertures afin de pouvoir procéder à l'achat et récupérer votre livre";
-
-                    $headersAdmin = "From: ".$email_from2. "\r\n" . "Reply-To: ".$email_from2. "\r\n" .
-                    'X-Mailer: PHP/' . phpversion();
-                    mail($email_to1, $email_subject, $emailAdmin, $headersAdmin);
-                } else {
-                    
-                    echo 'Mail failed';
-                } */
-    if (isset($_POST['submit'])) {
-        $to = "bouhalassak@gmail.com";
-        $to2 = "info@livreeclat.com";
-        $de = $_POST['courriel'];
-        $prenom = $_POST['prenom'];
-        $nom = $_POST['nom'];
-        $subject = "Nouvelle réservation";
-        $subject2 = "Confirmation de votre réservation";
-        $message = 'Vous avez reçu une nouvelle réservation pour le livre' . $livres['Titre'] . 'de' . $prenom . $nom;
-        $message2 = "Merci d'avoir choisi Livre Éclat. Votre livre" . $livres['Titre'] . "est maintenant réservé pour 30 jours.
-                    Vous pouvez passer en magasin durant nos heures d'ouvertures afin de pouvoir procéder à l'achat et récupérer votre livre";
-        /* $headers = "MIME-Version: 1.0" . "\r\n";
-                $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n"; */
-        $headers = "From:" . $de . "\r\n" . "Reply-To:" . $de . "\r\n";
-        mail($to, $subject, $message, $headers);
-        $headers2 = "From:" . $to . "\r\n" . "Reply-To:" . $to . "\r\n";
-        mail($to2, $subject2, $message2, $headers2);
-        // sends a copy of the message to the sender
-
+    $to = $_POST['courriel'];
+    $sujet = "Confirmation de votre réservation";
+    $sujetAdmin = "Nouvelle réservation";
+    $headers = "From: $emailAdmin\r\n";
+    $headers .= "Reply-To: $emailAdmin\r\n";
+    $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+    $message = nl2br(nl2br("Merci d'avoir choisi Livre Éclat. \r\nVotre livre " . $livres['Titre'] . " est maintenant réservé pour 30 jours.
+    Vous pouvez passer en magasin durant nos heures d'ouvertures afin de pouvoir procéder à l'achat et récupérer votre livre."));
+    $headersAdmin = "From: $email\r\n";
+    $headersAdmin .= "Reply-To: $email\r\n";
+    $headersAdmin .= "Content-Type: text/html; charset=UTF-8\r\n";
+    $messageAdmin = "Vous avez reçu une nouvelle réservation pour le livre " . $livres['Titre'] . ' de ' . $prenom . " " . $nom . ".";
+}
+    if (mail($email, $sujet, $message, $headers ) && mail($toAdmin, $sujetAdmin, $messageAdmin, $headersAdmin)) {
     ?>
-
+    <section id="confirmation">
+    <p style="color: green">Le courriel est envoyé</p>
     <?php } else {
 
     ?>
-        <section id="confirmation">
             <p style="color: red">L'envoi du courriel a échoué</p>
         <?php
     }
@@ -76,7 +49,7 @@ include './pages/partials/header.php';
         <h2>Confirmation réservation</h2>
         <p>Merci d'avoir choisi Livre Éclat. Votre livre <?= $livres['Titre'] ?> est maintenant réservé pour 30 jours. <br>
             Vous pouvez passer en magasin durant nos heures d'ouvertures afin de pouvoir procéder à l'achat et récupérer votre livre</p>
-        <p>Vous serez redirigé à la boutique dans <span id="counter" style="color: red">10</span>.</p>
+        <p>Vous serez redirigé à la boutique dans <span id="counter" style="color: red">30</span>.</p>
         </section>
 </main>
 <?php
--- src/resources/javascript/reservation.js
+++ src/resources/javascript/reservation.js
@@ -1,3 +1,5 @@
+// Redirection veuillez nous contacter
+
 document.querySelector('a[href="#adresse"]').addEventListener('click', function(event) {
     event.preventDefault();
     document.getElementById('adresse').scrollIntoView({
